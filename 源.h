9fa0599d10bd8166e5c08928b6e5f44b10d49e96
seekermm <49446214+seekermm@users.noreply.github.com>
2020-02-15 11:19:26 +0800

    6.12

--- 源.h
+++ 源.h
@@ -2,17 +2,12 @@
 #pragma warning(disable:4996)
 int main()
 {
-	float i=-1,n=1,sum_1=0, sum_2=0;
-	int limit;
-	scanf("%d", &limit);
-	while (n<=limit)
+	int integer[8] = { 2 }, n=0,i;
+	for (i = 0;i <= 6;i++)
+		integer[i + 1] = integer[i] * 2;
+	do
 	{
-		sum_1 += 1.0 /n ;
-		i = (-1)* i;
-		sum_2 += i/ n;
+		printf("%d ", integer[n]);
 		n++;
-	}
-	printf("%f\n", sum_1);
-	printf("%f\n", sum_2);
-	printf("%f\n", sum_1 + sum_2);
+	} while (n<=7);
 }
